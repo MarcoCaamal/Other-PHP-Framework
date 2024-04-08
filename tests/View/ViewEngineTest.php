947ece20ee85c566ef0addad5f8fced6785494c4
MarcoCaamal <caamalmarco99@gmail.com>
2024-04-07 23:30:31 -0500

    View engine test

---
+++ tests/View/ViewEngineTest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Junk\Tests\View;
+
+use Junk\View\ViewEngine;
+use PHPUnit\Framework\TestCase;
+
+class ViewEngineTest extends TestCase
+{
+    public function testRendersTemplateWithParameters()
+    {
+        $parameter1 = 'Test1';
+        $parameter2 = 2;
+
+        $expected = "
+            <html>
+                <body>
+                    <h1>$parameter1</h1>
+                    <h1>$parameter2</h1>
+                </body>
+            </html>
+        ";
+
+        $engine = new ViewEngine(__DIR__ . '/views');
+
+        $content = $engine->render('test', compact('parameter1', 'parameter2'), 'layout');
+
+        $this->assertEquals(
+            preg_replace("/\s/", "", $expected),
+            preg_replace("/\s/", "", $content)
+        );
+    }
+}
